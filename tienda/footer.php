6576595e55ead95c0d573638671e4c8cdd224569
Ángel Torres <129797801+T0rrag@users.noreply.github.com>
2025-11-26 00:44:05 +0100

    Delete tienda directory

--- tienda/footer.php
+++
@@ -1,8 +0,0 @@
-<hr>
-<footer>
-    <p>&copy; 2025 - Tienda Online</p>
-</footer>
-
-</body>
-</html>
-
--- tienda/inicio.php
+++
@@ -1,9 +0,0 @@
-<?php include "header.php"; ?>
-
-<h2>Bienvenido a la tienda</h2>
-<p>Selecciona una categoría para comenzar a comprar.</p>
-
-<a class="boton" href="categorias.php">Ver categorías</a>
-
-<?php include "footer.php"; ?>
-
